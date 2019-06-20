1687d921bc0fa3d68eab252e24a1c34a43c77ec2
sagar acharya <sagarach65@gmail.com>
2019-06-20 14:31:39 +0545

    first commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Person struct {
+	ID            string `json:"id"`
+	FirstName     string `json:"firstName"`
+	LastName      string `json:"lastName"`
+	MiddleName    string `json:"middleName"`
+	Age           int    `json:"age"`
+	EmailAddress  string `json:"emailAddress"`
+	MaritalStatus string `json:"maritalStatus"`
+}
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "sagar"
+	dbname   = "GO"
+)
+
+func main() {
+	r := gin.Default()
+	//defining routes
+	r.GET("/", GetUser)
+
+	fmt.Println("Server is up ")
+	// Start server
+	r.Run(":6000")
+}
---
+++ user.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetUser(c *gin.Context) {
+	//opening db
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	rows, err := db.Query(`SELECT * from users`)
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+	person := []Person{}
+	// fmt.Println(rows)
+
+	for rows.Next() {
+		var p Person
+		if err := rows.Scan(&p.ID, &p.FirstName, &p.LastName, &p.MiddleName, &p.Age, &p.EmailAddress, &p.MaritalStatus); err != nil {
+			panic(err)
+
+		}
+		person = append(person, p)
+	}
+
+	// Returns the result as json
+	c.JSON(200, gin.H{
+		data: person,
+	})
+}
